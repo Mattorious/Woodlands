95a3c4a7cf5635ec3564d29c517abc68f05c05d3
NeogearNeon <carter.shipes@gmail.com>
2021-02-15 21:30:52 -0500

    Unit Registry - Phase 2
    It's in place and imported like a charm. UI changes are here along with a few optimizations so I don't lose my mind.

---
+++ Source/Woodlands/MyDataTable.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "MyDataTable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WOODLANDS_API UMyDataTable : public UDataTable
+{
+	GENERATED_BODY()
+	
+};
